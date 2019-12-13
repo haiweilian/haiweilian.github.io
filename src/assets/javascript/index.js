e58ccfff256599c994ea8232cefb02f4c1e58ade
haiweilian <haiweilian@foxmail.com>
2019-12-13 09:10:00 +0800

    添加打字效果

---
+++ src/assets/javascript/index.js
@@ -0,0 +1,9 @@
+/* typed
+-------------------------- */
+new Typed ('#typed', {
+    strings: ['Welcome to HaiWei Lian.'],
+    typeSpeed: 60,
+    onComplete: function () {
+        document.querySelector('.typed-cursor').style.display = 'none'
+    }
+})
