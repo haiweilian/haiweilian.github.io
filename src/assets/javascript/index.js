a017b1ad6ae0b9ed590f1d155ea05405685d3367
haiweilian <haiweilian@foxmail.com>
2020-01-18 17:48:59 +0800

    发布存静态展示

--- src/assets/javascript/index.js
+++
@@ -1,9 +0,0 @@
-/* typed
--------------------------- */
-new Typed ('#typed', {
-    strings: ['Welcome to HaiWei Lian.'],
-    typeSpeed: 60,
-    onComplete: function () {
-        document.querySelector('.typed-cursor').style.display = 'none'
-    }
-})
